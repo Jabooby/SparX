379c8b67bd3232c29df53c755316f9bce26f0fb5
Jabooby <turcottejacob99@gmail.com>
2023-10-05 09:30:12 -0400

    Ajout code brampton

--- Defi_Parcours/Equipe_1/Robot_parcours/src/main.cpp
+++ Defi_Parcours/Equipe_1/Robot_parcours/src/main.cpp
@@ -56,7 +56,6 @@ struct PID {
   float ki = 0.0;
   float kd = 0.0;
   //variables de calcul PID
-  float counter = 0;
   float errsum  = 0.0;
   float prevErr = 0.0;
   float errdiff = 0;
@@ -77,19 +76,42 @@ struct robot {
 struct robot sparx; //création de la valeur global SparX. SparX est le robot et nous pouvons accéder
 // les différentes fonctions du robot. Pour accéder le moteur gauche juste faire "Sparx.vitesse_moteur_gauche"
 //variables timer
+int counter = 0;
 int timer = 0;
 int reset = 0;
 int interval = 50; //50 mS
 /************************* DECLARATIONS DE FONCTIONS *************************/
 int myFunction(int, int); //ceci est un exemple
+void arret();
+void avance();
+void recule();
+void tourneDroit();
+void tourneGauche();
+void PID();
+
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  BoardInit();
+  sparx.pid.errsum = 0.0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //compteur de chaque itération du loop
+  counter++;
+  //lecture du clock
+  timer = millis();
+  //calcul des données après chaque 50 mS
+  if ((timer-reset) > interval){
+    PID(); //calculs PID
+    avance();
+    reset = timer; //recommencer la clock
+  }
+  //Capture de données pendant 50 mS
+  else if ((timer-reset) <= interval){
+    avance();
+  }
 }
 
 /*
@@ -100,4 +122,56 @@ void loop() {
  */
 int myFunction(int x, int y) {
   return x + y;
+}
+
+void arret(){
+  MOTOR_SetSpeed(RIGHT, 0);
+  MOTOR_SetSpeed(LEFT, 0);
+};
+void avance(){
+  MOTOR_SetSpeed(RIGHT,sparx.vitesse_moteur_droite);
+  MOTOR_SetSpeed(LEFT, sparx.vitesse_moteur_gauche);
+};
+
+void recule(){
+  MOTOR_SetSpeed(RIGHT,-sparx.vitesse_moteur_droite);
+  MOTOR_SetSpeed(LEFT, -sparx.vitesse_moteur_gauche);
+};
+
+void tourneDroit(){
+  MOTOR_SetSpeed(RIGHT,0.5*sparx.vitesse_moteur_droite);
+  MOTOR_SetSpeed(LEFT, -0.5*sparx.vitesse_moteur_gauche);
+};
+
+void tourneGauche(){
+  MOTOR_SetSpeed(RIGHT, -0.5*sparx.vitesse_moteur_droite);
+  MOTOR_SetSpeed(LEFT, 0.5*sparx.vitesse_moteur_gauche);
+};
+
+void PID(){
+    //lire les valeurs des encodeurs
+    float left = ENCODER_Read(0);
+    float right = ENCODER_Read(1);
+    //calcul d'erreur
+    float error = right - left;
+    //calcul propotionelle
+    float proportionalValue = error * sparx.pid.kp;
+    //calcul intégrale
+    sparx.pid.errsum += error;
+    float integralValue = sparx.pid.ki * sparx.pid.errsum;
+    //calcul dérivé
+    float errdiff = error-sparx.pid.prevErr;
+    sparx.pid.prevErr = error;
+    float derivativeValue = sparx.pid.kd*errdiff;
+    //si la moteur gauche est plus lente
+    if (error > 0){
+      sparx.vitesse_moteur_gauche = sparx.vitesse + proportionalValue + integralValue + derivativeValue;
+
+    } 
+    //si la moteur gauche est plus vite
+    else if (error < 0) {
+      sparx.vitesse_moteur_gauche = sparx.vitesse - (proportionalValue + integralValue + derivativeValue);
+      
+    }
+    else sparx.vitesse = sparx.vitesse;
 }
