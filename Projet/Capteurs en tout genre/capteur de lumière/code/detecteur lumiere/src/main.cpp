8fb55dee9ed704946ced99c22c4db95d8806ead0
jeje408 <jelefebvre1@hotmail.com>
2023-11-12 16:54:51 -0500

    capteur de lumiere

---
+++ Projet/Capteurs en tout genre/capteur de lumière/code/detecteur lumiere/src/main.cpp
@@ -0,0 +1,47 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int DetecteurLumiere1();
+int DetecteurLumiere2();
+int DetecteurLumiere3();
+int DetecteurLumiere4();
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A8, INPUT);
+  pinMode(A9, INPUT);
+  pinMode(A10, INPUT);
+  pinMode(A11, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  DetecteurLumiere1();
+Serial.println(analogRead(A8));
+}
+
+// put function definitions here:
+int DetecteurLumiere1() {
+  int lumiere;
+  lumiere=analogRead(A8);
+  return lumiere;
+}
+
+int DetecteurLumiere2() {
+  int lumiere;
+  lumiere=analogRead(A9);
+  return lumiere;
+}
+
+int DetecteurLumiere3() {
+  int lumiere;
+  lumiere=analogRead(A10);
+  return lumiere;
+}
+
+int DetecteurLumiere4() {
+  int lumiere;
+  lumiere=analogRead(A11);
+  return lumiere;
+}
